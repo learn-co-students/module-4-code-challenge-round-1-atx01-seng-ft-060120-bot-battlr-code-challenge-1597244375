41c57d4eff7bd96231222d25c3fdff02fe23e63b
smithspencer817 <smithspencer817@gmail.com>
2020-08-12 10:47:06 -0500

    reached checkpoint

--- src/containers/BotsPage.js
+++ src/containers/BotsPage.js
@@ -1,12 +1,49 @@
 import React from "react";
+import YourBotArmy from "./YourBotArmy";
+import BotCollection from "./BotCollection";
+
+const API = "https://bot-battler-api.herokuapp.com/api/v1/bots"
 
 class BotsPage extends React.Component {
   //start here with your code for step one
 
+  state = {
+    bots: [],
+    botArmy: []
+  }
+
+  componentDidMount() {
+    fetch(API)
+    .then(res => res.json())
+    .then(bots => this.setState({
+      bots
+    }))
+  }
+
+  addBotToArmy = (id) => {
+    const bot = this.state.bots.find(b => b.id === id)
+    if (!this.state.botArmy.includes(bot)) {
+      this.setState({
+        botArmy: [...this.state.botArmy, bot]
+      })
+    }
+  }
+
+  removeBotFromArmy = (id) => {
+    const bot = this.state.bots.find(b => b.id === id)
+    const filteredArmy = this.state.botArmy.filter(b => b.id !== bot.id)
+    if (this.state.botArmy.includes(bot)) {
+      this.setState({
+        botArmy: filteredArmy
+      })
+    }
+  }
+
   render() {
     return (
       <div>
-        {/* put your components here */}
+        <YourBotArmy botArmy={this.state.botArmy} removeBotFromArmy={this.removeBotFromArmy}/>
+        <BotCollection bots={this.state.bots} addBotToArmy={this.addBotToArmy}/>
       </div>
     );
   }
--- src/containers/YourBotArmy.js
+++ src/containers/YourBotArmy.js
@@ -1,5 +1,5 @@
 import React from "react";
-import BotCard from "../components/BotCard";
+import ArmyBotCard from "../components/ArmyBotCard";
 
 class YourBotArmy extends React.Component {
   //your bot army code here...
@@ -9,8 +9,7 @@ class YourBotArmy extends React.Component {
       <div className="ui segment inverted olive bot-army">
         <div className="ui five column grid">
           <div className="row bot-army-row">
-            {/*...and here...*/}
-            Your Bot Army
+            {this.props.botArmy.map(bot => <ArmyBotCard bot={bot} key={Math.random()} removeBotFromArmy={this.props.removeBotFromArmy}/>)}
           </div>
         </div>
       </div>
